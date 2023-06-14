fb6776644d1cb77030c2d330b453d0e0808670bd
lucasmanea21 <lucasmanea@Yahoo.com>
2023-06-14 22:58:32 +0300

    fixed heroku port problem

--- model/main.py
+++ model/main.py
@@ -40,4 +40,5 @@ def query_index():
 
 if __name__ == "main":
     initialize_index()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
